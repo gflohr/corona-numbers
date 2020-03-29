91aa10b9691795fc980d401345757c3acc1975cc
Guido Flohr <guido.flohr@cantanea.com>
2020-03-29 15:30:30 +0300

    sort navigation

--- assets/scripts/dashboard.js
+++ assets/scripts/dashboard.js
@@ -1,1 +1,32 @@
-/* globals Chart:false, feather:false */
+$(function() {
+	// Sort the left navigation alphabetically.
+	var $links = $('.area-link'),
+		items = [];
+	
+	$links.each(function(index) {
+		items[index] = {
+			elem: $(this),
+			text: $(this).text(),
+			href: $(this).find('a').attr('href'),
+			klass: $(this).find('a').attr('class'),
+		}
+	});
+
+	items = items.sort(function(a, b) {
+		if (a.text < b.text) {
+			return -1;
+		} else if (a.text > b.text) {
+			return +1;
+		} else {
+			return 0;
+		}
+	});
+
+	$links.each(function(index) {
+		$(this).html('<a class="' + items[index].klass + '" href="'
+			+ items[index].href + '">' + items[index].text + '</a>'
+		);
+	});
+
+	console.log(items);
+});
